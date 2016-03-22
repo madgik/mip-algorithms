d204ab2d2f66144ca66a7c95f5a920963c9f6404
eleni <ezacharia@di.uoa.gr>
2016-03-22 17:14:37 +0200

    variable profiling  working for text v01 TODO : see global query

--- WP_VARIABLE_PROFILE/local.template.sql
+++ WP_VARIABLE_PROFILE/local.template.sql
@@ -6,15 +6,19 @@ select __rid as rid, __colname as colname, tonumber(__val)  as val
 from %{input_local_tbl}
 where colname = '%{variable}';
 
-var 'categorical' from
-select case when (select count(distinct val) from inputlocaltbl)< 20 then "True" else "False" end;
 
-var 'valIsText' from
-select case when (select typeof(val) from inputlocaltbl limit 1) ='text' then "True" else "False" end;
 
+var 'categorical' from select case when (select count(distinct val) from inputlocaltbl)< 20 then "True" else "False" end;
+var 'valIsText' from select case when (select typeof(val) from inputlocaltbl limit 1) ='text' then "True" else "False" end;
+
+
+
+--1. case when  val is a categorical number
 select *
 from ( select colname,
               val,
+              val as minval,
+              val as maxval,
               FSUM(val) as S1,
               FSUM(FARITH('*', val, val)) as S2,
               count(val) as N
@@ -24,9 +28,12 @@ from ( select colname,
 )
 
 union all
+--2. case when val is a number but not categorical
 select *
 from ( select colname,
               val,
+              min(val) as minval,
+              max(val) as maxval,
               FSUM(val) as S1,
               FSUM(FARITH('*', val, val)) as S2,
               count(val) as N
@@ -35,28 +42,35 @@ from ( select colname,
 )
 
 union all
+--3. case when val is text
 select *
 from ( select colname,
               val,
+              "NA" as minval,
+              "NA" as maxval,
               1 as S1,
               1 as S2,
               count(val) as N
        from ( select * from inputlocaltbl where '%{valIsText}'='True')
-             where val <> 'NA' and val is not null and val <> ""
-       where val is 'NA' or val is null or val == ""
+       where val <> 'NA' and val is not null and val <> ""
        group by val
 )
 
 union all
+--4. case when val is null
 select *
 from ( select colname,
               "NA" as val,
+              "NA" as minval,
+              "NA" as maxval,
                1 as S1,
                1 as S2,
               count(val) as N
        from inputlocaltbl
        where val is 'NA' or val is null or val == ""
 );
+
+
 
 
 
