5548b69fc229c26afa7f2156bd77bb827e864d32
vnikolopoulos <vgnikolop@di.uoa.gr>
2018-03-21 14:47:22 +0200

    adding pipeline regression tree

---
+++ PIPELINE_ISOUP_REGRESSION_TREE_SERIALIZER/global.template.sql
@@ -0,0 +1,34 @@
+requirevars 'prv_output_local_tbl' 'columns';
+
+drop table if exists columnstable;
+create table columnstable as
+  select strsplitv('%{columns}' ,'delimiter:,') as xname;
+
+
+var 'select_vars' from
+( select group_concat('"'||xname||'"',', ') as select_vars from columnstable);
+
+var 'var_count' from select count(*) from columnstable;
+
+create temp table data as select %{select_vars}, 0 as C1, 0 as C2, 0 as C3;
+
+select * from (output 'input.arff'
+               select "@attribute relation hour-weka.filters.unsupervised.attribute.Remove-R1-2" union all
+                      select "" union all select "@attribute "||column||" numeric" from (
+coltypes select * from data) union all
+                             select "" union all select "@data" );
+
+select writebinary('rtree.ser.prev', bin) from  %{prv_output_local_tbl};
+
+select execprogram(null, 'java', '-jar', 'ISOUPRegressionTreeSerializer.jar', 'input.arff', '1-%{var_count}', 'rtree.ser.prev');
+select execprogram(null, 'rm', 'input.arff');
+select execprogram(null, 'rm', c2) from dirfiles(.) where c2 like "rtree%prev";
+select execprogram(null,'rm',c2) from dirfiles(.) where c2 like "rtree%pfa.action.json";
+select execprogram(null,'rm',c2) from dirfiles(.) where c2 like "rtree%ser";
+
+var 'js_filename' from select c2 from dirfiles(.) where c2 like "rtree%vis.js";
+
+create temp table res_js as select group_concat(C1, " ") as res from (file '%{js_filename}');
+select execprogram(null, 'rm', '%{js_filename}');
+
+select res from res_js;
---
+++ PIPELINE_ISOUP_REGRESSION_TREE_SERIALIZER/localupdate.template.sql
@@ -0,0 +1,50 @@
+requirevars 'prv_output_local_tbl' 'columns' 'input_local_tbl';
+
+drop table if exists columnstable;
+create table columnstable as
+select strsplitv('%{columns}' ,'delimiter:,') as xname;
+
+create temp table localinputtbl_1 as
+select __rid as rid,__colname as colname, __val as val
+from %{input_local_tbl};
+
+--Check If variableS exist
+var 'counts' from select count(xname) from columnstable where xname in (select distinct(colname) from localinputtbl_1);		-->>By Sof
+var 'result' from select count(xname) from columnstable;						
+var 'valExists' from select case when(select %{counts})=%{result} then 1 else 0 end;			
+vars '%{valExists}'; 	
+--
+
+var 'select_vars' from
+( select group_concat('"'||xname||'"',', ') as select_vars from columnstable);
+
+var 'var_count' from select count(*) from columnstable;
+
+create temp table data as select %{select_vars}, 0 as C1, 0 as C2, 0 as C3  from (fromeav select * from localinputtbl_1);
+
+----Check if number of patients are more than minimum records----
+var 'minimumrecords' 10;
+create temp table emptytable as select * from data limit 0;
+var 'privacycheck' from select case when (select count(*) from data) < %{minimumrecords} then 0 else 1 end;
+create temp table safeData as 
+select * from data where %{privacycheck}=1
+union all
+select * from emptytable where %{privacycheck}=0;
+------
+
+select * from (output 'input.arff'
+               select "@attribute relation hour-weka.filters.unsupervised.attribute.Remove-R1-2" union all
+                      select "" union all select "@attribute "||column||" numeric" from (
+coltypes select * from safeData) union all
+                             select "" union all select "@data" union all select * from (csvout select * from safeData));
+
+select writebinary('rtree.ser.prev', bin) from  %{prv_output_local_tbl};
+
+select execprogram(null, 'java', '-jar', 'ISOUPRegressionTreeSerializer.jar', 'input.arff', '1-%{var_count}', 'rtree.ser.prev');
+select execprogram(null, 'rm', 'input.arff');
+select execprogram(null, 'rm', c2) from dirfiles(.) where c2 like "rtree%prev";
+select execprogram(null,'rm',c2) from dirfiles(.) where c2 like "rtree%pfa.action.json";
+select execprogram(null,'rm',c2) from dirfiles(.) where c2 like "rtree%vis.js";
+
+select bin from (unindexed select bin, execprogram(null,'rm',c2) from
+(unindexed select c2, execprogram(null,'cat',c2) as bin from dirfiles(.) where c2 like "rtree%ser"));
