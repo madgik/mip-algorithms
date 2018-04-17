75053939c24cdc947f0b5ba91b035a5756841ee7
sofiakarb <karvoun@di.uoa.gr>
2018-04-17 18:27:01 +0300

    Dataset selection/Fix error messages regarding if a variable exists/Empty fields give error

--- PIPELINE_ISOUP_MODEL_TREE_SERIALIZER/local.template.sql
+++ PIPELINE_ISOUP_MODEL_TREE_SERIALIZER/local.template.sql
@@ -1,5 +1,10 @@
 requirevars 'target_attributes' 'descriptive_attributes' 'input_local_tbl' ;
 
+------- Create the correct dataset 
+drop table if exists datasets;
+create table datasets as
+select strsplitv('%{dataset}','delimiter:,') as d;
+
 drop table if exists columnstable;
 create table columnstable as
 select strsplitv('%{target_attributes},%{descriptive_attributes}' ,'delimiter:,') as xname;
@@ -8,19 +13,32 @@ create temp table localinputtbl_1 as
 select __rid as rid,__colname as colname, __val as val
 from %{input_local_tbl};
 
---Check If variableS exist
-var 'counts' from select count(xname) from columnstable where xname in (select distinct(colname) from localinputtbl_1);		-->>By Sof
-var 'result' from select count(xname) from columnstable;						
+--Check if descriptive_attributes is empty
+var 'empty' from select case when (select '%{descriptive_attributes}')='' then 0 else 1 end;
+emptyfield '%{empty}';
+---------
+--Check if target_attributes is empty
+var 'empty' from select case when (select '%{target_attributes}')='' then 0 else 1 end;
+emptyfield '%{empty}';
+---------
+--Check if dataset is epmpty
+var 'empty' from select case when (select '%{dataset}')='' then 0 else 1 end;
+emptyset '%{empty}';
+------------------
+create table columnexist as setschema 'colname' select distinct(colname) from (postgresraw);
+--Check if columns exist
+var 'counts' from select count(distinct(colname)) from columnexist where colname in (select xname from columnstable);
+var 'result' from select count(xname) from columnstable;
 var 'valExists' from select case when(select %{counts})=%{result} then 1 else 0 end;			
-vars '%{valExists}'; 	
---
+vars '%{valExists}'; 
+------
 
 var 'select_vars' from
 ( select group_concat('"'||xname||'"',', ') as select_vars from columnstable);
 
 var 'target_var_count' from select count(*) from (select strsplitv('%{target_attributes}' ,'delimiter:,') as xname);
 
-create temp table data as select %{select_vars}  from (fromeav select * from localinputtbl_1);
+create temp table data as select %{select_vars}  from (fromeav select * from localinputtbl_1 where rid in (select rid from localinputtbl_1 where colname = 'dataset' and val in (select d from datasets)));
 
 ----Check if number of patients are more than minimum records----
 var 'minimumrecords' 10;
