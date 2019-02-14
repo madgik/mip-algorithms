ee4b949891c73216c7eb5f2ac4fdaaaae96c2c4f
Thanasis <tkarabatsis@hotmail.com>
2019-02-14 10:34:56 +0200

    Changes to read from file

--- WP_LIST_DATASET/local.template.sql
+++ WP_LIST_DATASET/local.template.sql
@@ -2,6 +2,8 @@ requirevars 'input_local_tbl';
 
 select execprogram(null, "/root/exareme/set-local-datasets.sh");
 
-var 'a' from select count(distinct(__rid)) as sum1 from (select distinct rid as __rid from(postgresraw dataset));
+var 'a' from select count(distinct(__rid)) as sum1 from (select distinct __rid from %{input_local_tbl});
+
 var 'b' from select execprogram(null,'cat','/root/exareme/etc/exareme/name');
-select var('a') as sum1, __val as val, var('b') as who from (select distinct val as __val from(postgresraw dataset)) group by __val;
+select var('a') as sum1, __val as val, var('b') as who from (select distinct __val from %{input_local_tbl} where __colname = 'dataset') group by __val;
+
