abed79e0e2d0a9989dc256c45ec58d865719e7f0
ezacharia <ezacharia@di.uoa.gr>
2019-03-19 17:49:00 +0200

    output format

--- ID3/finalize/1/global.template.sql
+++ ID3/finalize/1/global.template.sql
@@ -1,8 +1,11 @@
-requirevars 'defaultDB' ;
+requirevars 'defaultDB' 'outputformat';
+
+var 'defaultDB' 'defaultDB';
+var 'outputformat' 'pfa';
 attach database '%{defaultDB}' as defaultDB;
 
 --pfa format (json valid)
 setschema 'result'
 select * from (totabulardataresourceformat
-select no as `node id`,colname as `node colname`, val as `edge`,nextnode as `next node` from defaultDB.globaltree
+select no  as `node id`,colname as `node colname`, val as `edge`,nextnode as `next node` from defaultDB.globaltree
 ) where '%{outputformat}'= 'pfa';
