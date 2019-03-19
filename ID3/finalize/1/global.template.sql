106a213b7dfa0c6d6268b8f9d3e6b04c6a84a135
ezacharia <ezacharia@di.uoa.gr>
2019-03-19 17:22:24 +0200

    without output 2

--- ID3/finalize/1/global.template.sql
+++ ID3/finalize/1/global.template.sql
@@ -1,7 +1,6 @@
 requirevars 'defaultDB' ;
 
 --pfa format (json valid)
-select id3resultsviewer(nonode, nodecolname,edge,nextnode)
 setschema 'result'
 select * from (totabulardataresourceformat
 select no as `node id`,colname as `node colname`, val as `edge`,nextnode as `next node` from defaultDB.globaltree
