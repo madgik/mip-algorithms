f884c151662317d39d0e5fd91ab126bedf6efbfe
Jason Sakellariou <js.sakel@gmail.com>
2019-05-14 16:02:18 +0300

    Change minimentalstate to non-categorical

--- unit_tests/test_PearsonCorrelation.py
+++ unit_tests/test_PearsonCorrelation.py
@@ -8,109 +8,6 @@ import math
 endpointUrl = 'http://88.197.53.52:9090'
 
 
-def test_PearsonCorrelation_ADNI_9rows():
-    """
-    Results from R
-
-    > cor.test(X$leftaccumbensarea, Y$rightaccumbensarea, method="pearson")
-    cor = 0.832696403083016, p-value = 0.005332947202092
-
-    > cor.test(X$leftacgganteriorcingulategyrus, Y$rightacgganteriorcingulategyrus, method="pearson")
-    cor = 0.764766782355394, p-value = 0.016370022484567
-
-    > cor.test(X$leftainsanteriorinsula, Y$rightainsanteriorinsula, method="pearson")
-    cor = 0.928237609063798, p-value 0.000874899301446
-    """
-
-    logging.info("---------- TEST : Pearson Correlation ADNI on 9 rows")
-
-    data = [
-        {
-            "name" : "X",
-            "value": "leftaccumbensarea, leftacgganteriorcingulategyrus, leftainsanteriorinsula"
-        },
-        {
-            "name" : "Y",
-            "value": "rightaccumbensarea, rightacgganteriorcingulategyrus, rightainsanteriorinsula"
-        },
-        {
-            "name" : "dataset",
-            "value": "adni_9rows"
-        },
-        {
-            "name" : "filter",
-            "value": ""
-        }
-    ]
-
-    headers = {'Content-type': 'application/json', "Accept": "text/plain"}
-    r = requests.post(endpointUrl + '/mining/query/PEARSON_CORRELATION', data=json.dumps(data), headers=headers)
-
-    result = json.loads(r.text)
-
-    check_result(
-            result['result'][0], 'leftaccumbensarea_rightaccumbensarea', 0.832696403083016, 0.005332947202092
-    )
-    check_result(
-            result['result'][4], 'leftacgganteriorcingulategyrus_rightacgganteriorcingulategyrus',
-            0.764766782355394, 0.016370022484567
-    )
-    check_result(
-            result['result'][8], 'leftainsanteriorinsula_rightainsanteriorinsula', 0.928237609063798, 0.000874899301446
-    )
-
-
-def test_PearsonCorrelation_ADNI_alldata():
-    """
-    Results from R
-
-    > cor.test(X$leftaccumbensarea, Y$rightaccumbensarea, method="pearson")
-    cor = 0.911518956593483, p-value = 0.000000000000000
-
-    > cor.test(X$leftacgganteriorcingulategyrus, Y$rightacgganteriorcingulategyrus, method="pearson")
-    cor = 0.872706907353685, p-value = 0.000000000000000
-
-    > cor.test(X$leftainsanteriorinsula, Y$rightainsanteriorinsula, method="pearson")
-    cor = 0.907680160667781, p-value 0.000000000000000
-    """
-    logging.info("---------- TEST : Pearson Correlation ADNI on all data")
-
-    data = [
-        {
-            "name" : "X",
-            "value": "leftaccumbensarea, leftacgganteriorcingulategyrus, leftainsanteriorinsula"
-        },
-        {
-            "name" : "Y",
-            "value": "rightaccumbensarea, rightacgganteriorcingulategyrus, rightainsanteriorinsula"
-        },
-        {
-            "name" : "dataset",
-            "value": "adni"
-        },
-        {
-            "name" : "filter",
-            "value": ""
-        }
-    ]
-
-    headers = {'Content-type': 'application/json', "Accept": "text/plain"}
-    r = requests.post(endpointUrl + '/mining/query/PEARSON_CORRELATION', data=json.dumps(data), headers=headers)
-
-    result = json.loads(r.text)
-
-    check_result(
-            result['result'][0], 'leftaccumbensarea_rightaccumbensarea', 0.911518956593483, 0.000000000000000
-    )
-    check_result(
-            result['result'][4], 'leftacgganteriorcingulategyrus_rightacgganteriorcingulategyrus',
-            0.872706907353685, 0.000000000000000
-    )
-    check_result(
-            result['result'][8], 'leftainsanteriorinsula_rightainsanteriorinsula', 0.907680160667781, 0.000000000000000
-    )
-
-
 def test_PearsonCorrlation_MIP_AlgoTesting_1():
     """
     Results from 2019_MIP_Algo_Testing/PearsonCorrelation
@@ -195,46 +92,46 @@ def test_PearsonCorrlation_MIP_AlgoTesting_2():
     )
 
 
-# def test_PearsonCorrlation_MIP_AlgoTesting_2p1():
-#     """
-#     Results from 2019_MIP_Algo_Testing/PearsonCorrelation
-#
-#     subjectageyears vs minimentalstate
-#         Pearson's r     -0.149
-#         p-value         < .001
-#         95% CI Upper    -0.079
-#         95% CI Lower    -0.218
-#     """
-#
-#     logging.info("---------- TEST : Pearson Correlation MIP_Algo_Testing_2p1")
-#
-#     data = [
-#         {
-#             "name" : "X",
-#             "value": "subjectageyears"
-#         },
-#         {
-#             "name" : "Y",
-#             "value": "minimentalstate"
-#         },
-#         {
-#             "name" : "dataset",
-#             "value": "desd-synthdata"
-#         },
-#         {
-#             "name" : "filter",
-#             "value": ""
-#         },
-#     ]
-#
-#     headers = {'Content-type': 'application/json', "Accept": "text/plain"}
-#     r = requests.post(endpointUrl + '/mining/query/PEARSON_CORRELATION', data=json.dumps(data), headers=headers)
-#
-#     result = json.loads(r.text)
-#
-#     check_result(
-#             result['result'][0], 'subjectageyears_minimentalstate', -0.149, 0.000000000000000
-#     )
+def test_PearsonCorrlation_MIP_AlgoTesting_2p1():
+    """
+    Results from 2019_MIP_Algo_Testing/PearsonCorrelation
+
+    subjectageyears vs minimentalstate
+        Pearson's r     -0.149
+        p-value         < .001
+        95% CI Upper    -0.079
+        95% CI Lower    -0.218
+    """
+
+    logging.info("---------- TEST : Pearson Correlation MIP_Algo_Testing_2p1")
+
+    data = [
+        {
+            "name" : "X",
+            "value": "subjectageyears"
+        },
+        {
+            "name" : "Y",
+            "value": "minimentalstate"
+        },
+        {
+            "name" : "dataset",
+            "value": "desd-synthdata"
+        },
+        {
+            "name" : "filter",
+            "value": ""
+        },
+    ]
+
+    headers = {'Content-type': 'application/json', "Accept": "text/plain"}
+    r = requests.post(endpointUrl + '/mining/query/PEARSON_CORRELATION', data=json.dumps(data), headers=headers)
+
+    result = json.loads(r.text)
+
+    check_result(
+            result['result'][0], 'subjectageyears_minimentalstate', -0.149, 0.000000000000000
+    )
 
 
 def test_PearsonCorrlation_MIP_AlgoTesting_3():
