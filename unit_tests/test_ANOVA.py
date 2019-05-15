1658638f828517a2d3a9940eda8b4a7ee035bb75
ezacharia <ezacharia@di.uoa.gr>
2019-05-15 15:51:18 +0300

    un-comment test1

--- unit_tests/test_ANOVA.py
+++ unit_tests/test_ANOVA.py
@@ -7,51 +7,51 @@ from decimal import *
 
 url='http://localhost:9090/mining/query/ANOVA'
 
-#
-# def test_ANOVA_1():
-#     logging.info("---------- TEST : ANOVA - Tests With Balanced data set(data_ANOVA_Balanced_with_inter_V1V2.csv) - Test the 3 main effects and all the interactions with type I ANOVA.  ")
-#
-#     data = [
-#             {   "name": "iterations_max_number", "value": "20" },
-#             {   "name": "iterations_condition_query_provided", "value": "true" },
-#             {   "name": "x", "value": "ANOVA_var_I1*ANOVA_var_I2*ANOVA_var_I3" },
-#             {   "name": "y", "value": "ANOVA_var_D" },
-#             {   "name": "type", "value": "1" },
-#             {   "name": "dataset", "value": "ANOVA_Balanced_with_inter_V1V2" },
-#             {   "name": "filter", "value": "" },
-#             {   "name": "outputformat", "value": "pfa" }
-#         ]
-#
-#     headers = {'Content-type': 'application/json', "Accept": "text/plain"}
-#     r = requests.post(url,data=json.dumps(data),headers=headers)
-#     result = json.loads(r.text)
-#     print (r.text)
-#
-#     # exaremeresult ='{"resources": [{ "name": "ANOVA_TABLE","profile": "tabular-data-resource","data": [ ["model variables","sum of squares","Df","mean square","f","p","eta squared","part eta squared","omega squared" ],["ANOVA_var_I1",313447.744445,2,156723.872223,77571.8524291,3.05165932445e-242,0.955176956351,0.998956896013,0.955158762254],["ANOVA_var_I2",14193.611111,2,7096.8055555,3512.62603114,3.88260499978e-134,0.0432525373077,0.977460092036,0.0432399576434],["ANOVA_var_I3",113.605555,1,113.605555,56.2300638863,4.00217903518e-12,0.000346192978487,0.257664149865,0.000340034161086],["ANOVA_var_I1:ANOVA_var_I2",63.122223,4,15.78055575,7.81072420257,8.81901178094e-06,0.000192353889641,0.161676818791,0.00016772596138],["ANOVA_var_I1:ANOVA_var_I3",4.81111099996,2,2.40555549998,1.19065075159,0.306669502129,1.46610158888e-05,0.014486449988,2.3475536263e-06],["ANOVA_var_I2:ANOVA_var_I3",5.87777700002,2,2.93888850001,1.45462858845,0.236520238058,1.79114932057e-05,0.017641563771,5.59801093111e-06],["ANOVA_var_I1:ANOVA_var_I2:ANOVA_var_I3",0.655556000012,4,0.163889000003,0.0811182951436,0.988064640922,1.99769178725e-06,0.00199891719478,-2.26290645103e-05],["residuals",327.3,162,2.02037037037]], "schema":  { "fields": [{"name": "model variables","type": "text"},{"name": "sum of squares","type": "number"},{"name": "Df","type": "number"},{"name": "mean square","type": "number"},{"name": "f","type": "number"},{"name": "p","type": "number"},{"name": "eta squared","type": "number"},{"name": "part eta squared","type": "number"},{"name": "omega squared","type": "number"} ]}}]}'
-#     # result = json.loads(exaremeresult)
-#
-#
-# ##  ANOVA
-# ##  ──────────────────────────────────────────────────────────────────────────────────────────────────────────
-# ##                            Sum of Squares    df     Mean Square    F             p         η²       η²p
-# ##  ─────────────────────────────────────────────────────────────────────────────────────────────────────────
-# ##    var_I1                      313447.744      2     156723.872    77571.8524    < .001    0.955    0.999
-# ##    var_I2                       14193.611      2       7096.806     3512.6260    < .001    0.043    0.977
-# ##    var_I3                         113.606      1        113.606       56.2301    < .001    0.000    0.258
-# ##    var_I1:var_I2                   63.122      4         15.781        7.8107    < .001    0.000    0.162
-# ##    var_I1:var_I3                    4.811      2          2.406        1.1907     0.307    0.000    0.014
-# ##    var_I2:var_I3                    5.878      2          2.939        1.4546     0.237    0.000    0.018
-# ##    var_I1:var_I2:var_I3             0.656      4          0.164        0.0811     0.988    0.000    0.002
-# ##    Residuals                      327.300    162          2.020
-#
-#     check_variable(result['resources'][0]['data'][1],'ANOVA_var_I1', 313447.744, 2, 156723.872, 77571.8524, '< .001', 0.955, 0.999)
-#     check_variable(result['resources'][0]['data'][2],'ANOVA_var_I2', 14193.611, 2, 7096.806, 3512.6260, '< .001', 0.043, 0.977)
-#     check_variable(result['resources'][0]['data'][3],'ANOVA_var_I3',113.606, 1, 113.606, 56.2301, '< .001', 0.000, 0.258 )
-#     check_variable(result['resources'][0]['data'][4],'ANOVA_var_I1:ANOVA_var_I2',63.122, 4, 15.781, 7.8107, '< .001',0.000,0.162 )
-#     check_variable(result['resources'][0]['data'][5],'ANOVA_var_I1:ANOVA_var_I3', 4.811, 2, 2.406, 1.1907, 0.307,0.000,0.014 )
-#     check_variable(result['resources'][0]['data'][6],'ANOVA_var_I2:ANOVA_var_I3',5.878, 2, 2.939, 1.4546, 0.237,0.000,0.018 )
-#     check_variable(result['resources'][0]['data'][7],'ANOVA_var_I1:ANOVA_var_I2:ANOVA_var_I3',0.656, 4, 0.164, 0.0811, 0.988,0.000,0.002)
-#     check_variable(result['resources'][0]['data'][8],'residuals',327.300, 162,  2.020)
+
+def test_ANOVA_1():
+    logging.info("---------- TEST : ANOVA - Tests With Balanced data set(data_ANOVA_Balanced_with_inter_V1V2.csv) - Test the 3 main effects and all the interactions with type I ANOVA.  ")
+
+    data = [
+            {   "name": "iterations_max_number", "value": "20" },
+            {   "name": "iterations_condition_query_provided", "value": "true" },
+            {   "name": "x", "value": "ANOVA_var_I1*ANOVA_var_I2*ANOVA_var_I3" },
+            {   "name": "y", "value": "ANOVA_var_D" },
+            {   "name": "type", "value": "1" },
+            {   "name": "dataset", "value": "ANOVA_Balanced_with_inter_V1V2" },
+            {   "name": "filter", "value": "" },
+            {   "name": "outputformat", "value": "pfa" }
+        ]
+
+    headers = {'Content-type': 'application/json', "Accept": "text/plain"}
+    r = requests.post(url,data=json.dumps(data),headers=headers)
+    result = json.loads(r.text)
+    print (r.text)
+
+    # exaremeresult ='{"resources": [{ "name": "ANOVA_TABLE","profile": "tabular-data-resource","data": [ ["model variables","sum of squares","Df","mean square","f","p","eta squared","part eta squared","omega squared" ],["ANOVA_var_I1",313447.744445,2,156723.872223,77571.8524291,3.05165932445e-242,0.955176956351,0.998956896013,0.955158762254],["ANOVA_var_I2",14193.611111,2,7096.8055555,3512.62603114,3.88260499978e-134,0.0432525373077,0.977460092036,0.0432399576434],["ANOVA_var_I3",113.605555,1,113.605555,56.2300638863,4.00217903518e-12,0.000346192978487,0.257664149865,0.000340034161086],["ANOVA_var_I1:ANOVA_var_I2",63.122223,4,15.78055575,7.81072420257,8.81901178094e-06,0.000192353889641,0.161676818791,0.00016772596138],["ANOVA_var_I1:ANOVA_var_I3",4.81111099996,2,2.40555549998,1.19065075159,0.306669502129,1.46610158888e-05,0.014486449988,2.3475536263e-06],["ANOVA_var_I2:ANOVA_var_I3",5.87777700002,2,2.93888850001,1.45462858845,0.236520238058,1.79114932057e-05,0.017641563771,5.59801093111e-06],["ANOVA_var_I1:ANOVA_var_I2:ANOVA_var_I3",0.655556000012,4,0.163889000003,0.0811182951436,0.988064640922,1.99769178725e-06,0.00199891719478,-2.26290645103e-05],["residuals",327.3,162,2.02037037037]], "schema":  { "fields": [{"name": "model variables","type": "text"},{"name": "sum of squares","type": "number"},{"name": "Df","type": "number"},{"name": "mean square","type": "number"},{"name": "f","type": "number"},{"name": "p","type": "number"},{"name": "eta squared","type": "number"},{"name": "part eta squared","type": "number"},{"name": "omega squared","type": "number"} ]}}]}'
+    # result = json.loads(exaremeresult)
+
+
+##  ANOVA
+##  ──────────────────────────────────────────────────────────────────────────────────────────────────────────
+##                            Sum of Squares    df     Mean Square    F             p         η²       η²p
+##  ─────────────────────────────────────────────────────────────────────────────────────────────────────────
+##    var_I1                      313447.744      2     156723.872    77571.8524    < .001    0.955    0.999
+##    var_I2                       14193.611      2       7096.806     3512.6260    < .001    0.043    0.977
+##    var_I3                         113.606      1        113.606       56.2301    < .001    0.000    0.258
+##    var_I1:var_I2                   63.122      4         15.781        7.8107    < .001    0.000    0.162
+##    var_I1:var_I3                    4.811      2          2.406        1.1907     0.307    0.000    0.014
+##    var_I2:var_I3                    5.878      2          2.939        1.4546     0.237    0.000    0.018
+##    var_I1:var_I2:var_I3             0.656      4          0.164        0.0811     0.988    0.000    0.002
+##    Residuals                      327.300    162          2.020
+
+    check_variable(result['resources'][0]['data'][1],'ANOVA_var_I1', 313447.744, 2, 156723.872, 77571.8524, '< .001', 0.955, 0.999)
+    check_variable(result['resources'][0]['data'][2],'ANOVA_var_I2', 14193.611, 2, 7096.806, 3512.6260, '< .001', 0.043, 0.977)
+    check_variable(result['resources'][0]['data'][3],'ANOVA_var_I3',113.606, 1, 113.606, 56.2301, '< .001', 0.000, 0.258 )
+    check_variable(result['resources'][0]['data'][4],'ANOVA_var_I1:ANOVA_var_I2',63.122, 4, 15.781, 7.8107, '< .001',0.000,0.162 )
+    check_variable(result['resources'][0]['data'][5],'ANOVA_var_I1:ANOVA_var_I3', 4.811, 2, 2.406, 1.1907, 0.307,0.000,0.014 )
+    check_variable(result['resources'][0]['data'][6],'ANOVA_var_I2:ANOVA_var_I3',5.878, 2, 2.939, 1.4546, 0.237,0.000,0.018 )
+    check_variable(result['resources'][0]['data'][7],'ANOVA_var_I1:ANOVA_var_I2:ANOVA_var_I3',0.656, 4, 0.164, 0.0811, 0.988,0.000,0.002)
+    check_variable(result['resources'][0]['data'][8],'residuals',327.300, 162,  2.020)
 
 def test_ANOVA_2():
     logging.info("---------- TEST : ANOVA - Tests With Balanced data set(data_ANOVA_Balanced_with_inter_V1V2.csv) - Test the 3 main effects and all the interactions with type II ANOVA.  ")
